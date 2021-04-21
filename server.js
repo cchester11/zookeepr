0b92813467888a7606a4f803a6641d7b41d6e52e
Charles <cchester1@angelo.edu>
2021-04-21 00:46:02 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const app = express();
+const PORT = 3001;
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  let filteredResults = animalsArray;
+  if (query.personalityTraitsArray) {
+    if (typeof query.personalityTraitsArray === 'string') {
+      personalityTraitsArray = [query.personalityTraitsArray];
+    } else {
+      personalityTraitsArray = query.personalityTraitsArray;
+    }
+    personalityTraitsArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraitsArray.indexOf(trait) !== 1
+      )
+    })
+  }
+  if(query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  console.log(req.query)
+  res.json(results);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port ${PORT}`)
+})
